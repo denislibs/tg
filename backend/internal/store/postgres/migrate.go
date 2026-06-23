f9faf24407c86730b9fa74b059277697ed0e52e2
@undefined_is_not_a_function <denismakste@gmail.com>
2026-06-23 20:56:18 +0500

    feat(backend): postgres pool and embedded migrations (users, devices, auth_codes)

---
+++ backend/internal/store/postgres/migrate.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
+
+// Migrate runs all up migrations against the database at databaseURL.
+func Migrate(databaseURL string) error {
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		return fmt.Errorf("open db for migrate: %w", err)
+	}
+	defer db.Close()
+
+	goose.SetBaseFS(migrationsFS)
+	if err := goose.SetDialect("postgres"); err != nil {
+		return err
+	}
+	return goose.Up(db, "migrations")
+}
